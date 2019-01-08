34b0bca69df4b6a206d368c7e276ac7760eadb5e
usbpendrive <ferry.fernando@gmail.com>
2019-01-08 07:32:30 +0700

    ToDo In Memory

---
+++ src/main/java/org/ragnarok/todo/todoinmemory/TodoInMemoryApplication.java
@@ -0,0 +1,14 @@
+package org.ragnarok.todo.todoinmemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoInMemoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoInMemoryApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/org/ragnarok/todo/todoinmemory/repository/ToDoRepository.java
@@ -0,0 +1,54 @@
+package org.ragnarok.todo.todoinmemory.repository;
+
+import org.ragnarok.todo.todoinmemory.domain.ToDo;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Repository
+public class ToDoRepository implements CommonRepository<ToDo> {
+    private Map<String, ToDo> toDos = new HashMap<>();
+
+    @Override
+    public ToDo save(ToDo domain) {
+        ToDo result = toDos.get(domain.getId());
+        if (result != null) {
+            result.setModified(LocalDateTime.now());
+            result.setDescription(domain.getDescription());
+            result.setCompleted(domain.isCompleted());
+            domain = result;
+        }
+        toDos.put(domain.getId(), domain);
+        return toDos.get(domain.getId());
+    }
+
+    @Override
+    public Iterable<ToDo> save(Collection<ToDo> domains) {
+        domains.forEach(this::save);
+        return findAll();
+    }
+
+    @Override
+    public void delete(ToDo domain) {
+        toDos.remove(domain.getId());
+    }
+
+    @Override
+    public ToDo findById(String id) {
+        return toDos.get(id);
+    }
+
+    @Override
+    public Iterable<ToDo> findAll() {
+        return toDos.entrySet().stream().sorted(entryComparator)
+                .map(Map.Entry::getValue).collect(Collectors.toList());
+    }
+
+    private Comparator<Map.Entry<String, ToDo>> entryComparator =
+            Comparator.comparing((Map.Entry<String, ToDo> o) -> o.getValue().getCreated());
+}
